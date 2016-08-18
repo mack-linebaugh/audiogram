c0294bfe3d34ddc5f48469613be320b426008d73
Brent Jones <bjones@stlpublicradio.org>
2016-08-18 14:51:32 -0500

    updates settings with aws bucket

--- settings/index.js
+++ settings/index.js
@@ -20,7 +20,7 @@ var path = require("path");
 
 module.exports = {
   workingDirectory: path.join(__dirname, "..", "tmp"),
-  storagePath: path.join(__dirname, "..", "media"),
+  s3Bucket: "stlpr-audiogram",
   fonts: [
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Regular.ttf") },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
