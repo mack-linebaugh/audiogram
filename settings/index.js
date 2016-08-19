a671b4fedf48b3409c9969ffc4ea8dcc6b601a08
Brent Jones <bjones@stlpublicradio.org>
2016-08-19 14:25:09 -0500

    adds font to index.js

--- settings/index.js
+++ settings/index.js
@@ -26,6 +26,7 @@ module.exports = {
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Bold.ttf"), weight: "bold" },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Italic.ttf"), style: "italic" },
-    { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-BoldItalic.ttf"), weight: "bold", style: "italic" }
+    { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-BoldItalic.ttf"), weight: "bold", style: "italic" },
+    { family: "Titillium Light", file: path.join(__dirname, "fonts", "Titillium-Light.ttf")  }
   ]
 };
